9e7a7a23cc71e3c03b901491f4a585056fe1dee2
Shea Rial <sheatrevor@gmail.com>
2012-03-21 17:07:56 -0400

    Added a link class.

---
+++ classes/kohana/ui/link.php
@@ -0,0 +1,14 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+/**
+ * Handles the rendering of a single link. Usually an 'a' tag, unless the
+ * corresponding view has been modified.
+ *
+ * @package    Kohana/UI
+ * @category   Extension
+ * @author     Kohana Team
+ * @copyright  (c) 2011-2012 Kohana Team
+ * @license    http://kohanaphp.com/license
+ */
+class Kohana_UI_Link {
+
+} // End Kohana_UI_Link
