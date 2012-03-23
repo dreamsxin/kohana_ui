0a7465005a71c758d17dcb9550e716f229778fa9
Shea Trevor <sheatrevor@gmail.com>
2012-03-23 10:52:34 -0700

    Work.

--- classes/kohana/ui/html/document.php
+++ classes/kohana/ui/html/document.php
@@ -11,10 +11,9 @@
 class Kohana_UI_HTML_Document extends UI_Container {
 
     /**
-     * @var  array  An array of all of the JavaScript resources to load
-     *              in the head section of the document.
+     * @var  string  Holds the page title.
      */
-    protected $_javascript = array();
+    protected $_title = NULL;
 
     /**
      * @var  array  An array of all of the CSS resources we would like to
@@ -23,9 +22,10 @@ class Kohana_UI_HTML_Document extends UI_Container {
     protected $_css = array();
 
     /**
-     * @var  string  Holds the page title.
+     * @var  array  An array of all of the JavaScript resources to load
+     *              in the head section of the document.
      */
-    protected $_title = NULL;
+    protected $_javascript = array();
 
     /**
      * Sets up the specific configuration items on this class instance.
@@ -37,9 +37,169 @@ class Kohana_UI_HTML_Document extends UI_Container {
         // Call the parent initialize method
         parent::_initialize();
 
-        // Copy the page title from the configuration data
-        $this->set_title(isset($this->configuration->title) ?
-            $this->configuration->title : '');
+        // If the configuration data has a title property
+        if (isset($this->configuration->title)) {
+            // Set the page title
+            $this->set_title($this->configuration->title);
+        }
+
+        // If the configuration data has a css property
+        if (isset($this->configuration->css)) {
+            // Set the CSS property
+            $this->set_css($this->configuration->css);
+        }
+
+        // If the configuration data has a javascript property
+        if (isset($this->configuration->javascript)) {
+            // Set the JavaScript property
+            $this->set_javascript($this->configuration->javascript);
+        }
+    }
+
+    /**
+     * Returns the current page title.
+     *
+     * @return  string  The page title value.
+     */
+    public function get_title()
+    {
+        // Return the title value
+        return $this->_title;
+    }
+
+    /**
+     * Sets the page title to the passed value.
+     *
+     * @param   string  The page title to assign.
+     * @return  object  A reference to this class instance.
+     */
+    public function set_title($title)
+    {
+        // Set the title value
+        $this->_title = $title;
+
+        // Return a reference to this class instance
+        return $this;
+    }
+
+    /**
+     * Sets one or more passed CSS paths, overwriting whatever was there.
+     *
+     * @param   mixed   A string with a single CSS URI or an array of them.
+     * @return  object  A reference to this class instance.
+     */
+    public function set_css($uris)
+    {
+        // Clear out the current CSS values
+        $this->_css = array();
+
+        // If the passed value is a string, make it an array with one item
+        $uris = is_string($uris) ? array($uris) : $uris;
+
+        // Loop over each of the passed uris
+        foreach ($uris as $uri) {
+            // Add this CSS URI
+            $this->add_css($uri);
+        }
+
+        // Return a reference to this class instance
+        return $this;
+    }
+
+    /**
+     * Adds a single CSS path to the css property.
+     *
+     * @param   string  The CSS URI or URI partial.
+     * @return  object  A reference to this class instance.
+     */
+    public function add_css($uri)
+    {
+        // Add the passed URI to the css property
+        $this->_css[$uri] = TRUE;
+    }
+
+    /**
+     * Removes a CSS URI from the css property.
+     *
+     * @param   string  The CSS URI or URI partial.
+     * @return  object  A reference to this class instance.
+     */
+    public function remove_css($uri)
+    {
+        // Remove the passed URI
+        unset($this->_css[$uri]);
+    }
+
+    /**
+     * Returns an array of CSS URIs.
+     *
+     * @return  array  An array with CSS URIs.
+     */
+    public function get_css()
+    {
+        // Return all of the CSS URIs in an array
+        return array_keys($this->_css);
+    }
+
+    /**
+     * Sets one or more passed JavaScript URIs, overwriting whatever
+     * was there.
+     *
+     * @param   mixed   A string with a single JavaScript URI, or an array
+     *                  with multiple JavaScript URIs.
+     * @return  object  A reference to this class instance.
+     */
+    public function set_javascript($uris)
+    {
+        // Clear out the current JavaScript values
+        $this->_javascript = array();
+
+        // If the passed value is a string, make it an array with one item
+        $uris = is_string($uris) ? array($uris) : $uris;
+
+        // Loop over each of the passed uris
+        foreach ($uris as $uri) {
+            // Add this JavaScript URI
+            $this->add_javascript($uri);
+        }
+
+        // Return a reference to this class instance
+        return $this;
+    }
+
+    /**
+     * Adds a single JavaScript URI.
+     *
+     * @param   string  The JavaScript URI or URI partial.
+     * @return  object  A reference to this class instance.
+     */
+    public function add_javascript($uri)
+    {
+        // Add the passed URI
+        $this->_javascript[$uri] = TRUE;
+    }
+
+    /**
+     * Removes a single JavaScript URI.
+     *
+     * @param   string  The JavaScript URI or URI partial.
+     * @return  object  A reference to this class instance.
+     */
+    public function remove_javascript($uri)
+    {
+        // Remove the passed URI
+        unset($this->_javascript[$uri]);
+    }
+
+    /**
+     * Returns an array of JavaScript URIs.
+     *
+     * @return  array  An array with JavaScript URIs.
+     */
+    public function get_javascript()
+    {
+        // Return all of the CSS URIs in an array
+        return array_keys($this->_javascript);
     }
 
 } // End Kohana_UI_HTML_Document
