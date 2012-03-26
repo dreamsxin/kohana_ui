ada15712c0643f96b4981bba4cadac4d8e8e859c
Shea Trevor <sheatrevor@gmail.com>
2012-03-26 14:44:39 -0700

    Moved this here.

--- views/ui/link.php
+++ views/ui/link.php
@@ -3,6 +3,9 @@
     // Echo the start tag
     echo '<a'.HTML::attributes($container->get_attributes()).'>';
 
+    // Echo the escaped link text, if there is any
+    echo HTML::entities($container->get_text());
+
     // Echo the result of rendering the children
     echo $container->render_children();
 
