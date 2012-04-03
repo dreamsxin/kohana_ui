521ef41b7ec35182682fc0c6558c19c7f8f3a8f1
Shea Trevor <sheatrevor@gmail.com>
2012-04-03 12:10:22 -0700

    Hidden form field with the form id.

--- views/ui/form.php
+++ views/ui/form.php
@@ -3,6 +3,13 @@
     // Echo the start tag
     echo '<form'.HTML::attributes($container->get_attributes()).'>';
 
+    // Create a hidden form field with the form id
+    echo '<input'.HTML::attributes(array(
+        'type' => 'hidden',
+        'name' => '_form_id',
+        'value' => (string) $container->get_id(),
+    )).' />';
+
     // Echo the result of rendering the children
     echo $container->render_children();
 
